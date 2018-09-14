c1ced643e57ed797676ba2df159cf8c84ada56c4
robertiqk <delux1945@gmail.com>
2018-09-14 17:20:15 +0300

    Exercise 3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+    this.chooseComparator = (a,b) => {
+      return a - b;
+    }
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+  return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    let sortArray = [];
+    for (let i = 0; i < indices.length; i++) {
+      sortArray.push(this.array[indices[i]] );
+    }
+    sortArray.sort(this.chooseComparator);
+    indices.sort();
+    for (let j = 0; j < sortArray.length; j++) {
+      this.array[indices[j]] = sortArray[j];
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.chooseComparator = compareFunction;
   }
 }
 
